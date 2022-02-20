917c0eccb70edd11cd6f534008d99e3567adea6a
mygoaway <mygoaway2@gmail.com>
2022-02-20 11:11:23 +0900

    logging 적용(롬복 - @Slf4j)

--- src/main/java/com/example/mysqlconnecttest/controller/UserController.java
+++ src/main/java/com/example/mysqlconnecttest/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.mysqlconnecttest.controller;
 
 import com.example.mysqlconnecttest.model.Users;
 import com.example.mysqlconnecttest.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -16,11 +18,13 @@ public class UserController {
 
     @PostMapping("/user")
     public Users create(@RequestBody Users users) {
+        log.debug("create() 호출됨");
         return userRepository.save(users);
     }
 
     @GetMapping("/user/{id}")
     public String read(@PathVariable Long id) {
+        log.debug("read() 호출됨");
         Optional<Users> userOptional = userRepository.findById(id);
         userOptional.ifPresent(System.out::println);
 
